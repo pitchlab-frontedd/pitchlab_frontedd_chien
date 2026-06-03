17c3f4c84201a80b83c0b8b82d0e59c63a2b986d
chien225071ss-gif <chien225071ss@gmail.com>
2026-06-03 23:18:42 +0800

    Align pitch tracking table styling

--- src/components/PitchTypeTable.jsx
+++ src/components/PitchTypeTable.jsx
@@ -72,10 +72,15 @@ const pctCell = value => (
   <span className="tracking-number">{pct(value)}</span>
 )
 
+const right = {
+  align: 'right',
+}
+
 const columns = [
   {
     title: metricTitle('Pitch Type'),
     dataIndex: 'pitchType',
+    align: 'left',
     width: 132,
     render: pt => (
       <span
@@ -93,25 +98,26 @@ const columns = [
     defaultSortOrder: 'descend',
     width: 64,
     render: numericCell,
+    ...right,
   },
-  { title: metricTitle('# RHB'), dataIndex: 'rhb', sorter: (a, b) => (a.rhb || 0) - (b.rhb || 0), width: 74, render: numericCell },
-  { title: metricTitle('# LHB'), dataIndex: 'lhb', sorter: (a, b) => (a.lhb || 0) - (b.lhb || 0), width: 74, render: numericCell },
-  { title: metricTitle('%'), dataIndex: 'pct', sorter: (a, b) => a.pct - b.pct, width: 62, render: pctCell },
-  { title: metricTitle('MPH'), dataIndex: 'mph', sorter: (a, b) => (a.mph || 0) - (b.mph || 0), width: 66, render: value => <span className="tracking-number">{oneDecimal(value)}</span> },
-  { title: metricTitle('PA'), dataIndex: 'pa', sorter: (a, b) => (a.pa || 0) - (b.pa || 0), width: 56, render: numericCell },
-  { title: metricTitle('AB'), dataIndex: 'ab', sorter: (a, b) => (a.ab || 0) - (b.ab || 0), width: 56, render: numericCell },
-  { title: metricTitle('H'), dataIndex: 'h', sorter: (a, b) => (a.h || 0) - (b.h || 0), width: 48, render: numericCell },
-  { title: metricTitle('1B'), dataIndex: 'singles', sorter: (a, b) => (a.singles || 0) - (b.singles || 0), width: 50, render: numericCell },
-  { title: metricTitle('2B'), dataIndex: 'doubles', sorter: (a, b) => (a.doubles || 0) - (b.doubles || 0), width: 50, render: numericCell },
-  { title: metricTitle('3B'), dataIndex: 'triples', sorter: (a, b) => (a.triples || 0) - (b.triples || 0), width: 50, render: numericCell },
-  { title: metricTitle('HR'), dataIndex: 'hr', sorter: (a, b) => (a.hr || 0) - (b.hr || 0), width: 52, render: numericCell },
-  { title: metricTitle('SO'), dataIndex: 'so', sorter: (a, b) => (a.so || 0) - (b.so || 0), width: 52, render: numericCell },
-  { title: metricTitle('BBE'), dataIndex: 'bbe', sorter: (a, b) => (a.bbe || 0) - (b.bbe || 0), width: 60, render: numericCell },
-  { title: metricTitle('BA'), dataIndex: 'ba', sorter: (a, b) => (a.ba || 0) - (b.ba || 0), width: 64, render: rateCell },
-  { title: metricTitle('SLG'), dataIndex: 'slg', sorter: (a, b) => (a.slg || 0) - (b.slg || 0), width: 68, render: rateCell },
-  { title: metricTitle('wOBA'), dataIndex: 'woba', sorter: (a, b) => (a.woba || 0) - (b.woba || 0), width: 76, render: rateCell },
-  { title: metricTitle('Whiff%'), dataIndex: 'whiffPct', sorter: (a, b) => a.whiffPct - b.whiffPct, width: 82, render: pctCell },
-  { title: metricTitle('PutAway%'), dataIndex: 'putAwayPct', sorter: (a, b) => (a.putAwayPct || 0) - (b.putAwayPct || 0), width: 96, render: pctCell },
+  { title: metricTitle('# RHB'), dataIndex: 'rhb', sorter: (a, b) => (a.rhb || 0) - (b.rhb || 0), width: 74, render: numericCell, ...right },
+  { title: metricTitle('# LHB'), dataIndex: 'lhb', sorter: (a, b) => (a.lhb || 0) - (b.lhb || 0), width: 74, render: numericCell, ...right },
+  { title: metricTitle('%'), dataIndex: 'pct', sorter: (a, b) => a.pct - b.pct, width: 62, render: pctCell, ...right },
+  { title: metricTitle('MPH'), dataIndex: 'mph', sorter: (a, b) => (a.mph || 0) - (b.mph || 0), width: 66, render: value => <span className="tracking-number">{oneDecimal(value)}</span>, ...right },
+  { title: metricTitle('PA'), dataIndex: 'pa', sorter: (a, b) => (a.pa || 0) - (b.pa || 0), width: 56, render: numericCell, ...right },
+  { title: metricTitle('AB'), dataIndex: 'ab', sorter: (a, b) => (a.ab || 0) - (b.ab || 0), width: 56, render: numericCell, ...right },
+  { title: metricTitle('H'), dataIndex: 'h', sorter: (a, b) => (a.h || 0) - (b.h || 0), width: 48, render: numericCell, ...right },
+  { title: metricTitle('1B'), dataIndex: 'singles', sorter: (a, b) => (a.singles || 0) - (b.singles || 0), width: 50, render: numericCell, ...right },
+  { title: metricTitle('2B'), dataIndex: 'doubles', sorter: (a, b) => (a.doubles || 0) - (b.doubles || 0), width: 50, render: numericCell, ...right },
+  { title: metricTitle('3B'), dataIndex: 'triples', sorter: (a, b) => (a.triples || 0) - (b.triples || 0), width: 50, render: numericCell, ...right },
+  { title: metricTitle('HR'), dataIndex: 'hr', sorter: (a, b) => (a.hr || 0) - (b.hr || 0), width: 52, render: numericCell, ...right },
+  { title: metricTitle('SO'), dataIndex: 'so', sorter: (a, b) => (a.so || 0) - (b.so || 0), width: 52, render: numericCell, ...right },
+  { title: metricTitle('BBE'), dataIndex: 'bbe', sorter: (a, b) => (a.bbe || 0) - (b.bbe || 0), width: 60, render: numericCell, ...right },
+  { title: metricTitle('BA'), dataIndex: 'ba', sorter: (a, b) => (a.ba || 0) - (b.ba || 0), width: 64, render: rateCell, ...right },
+  { title: metricTitle('SLG'), dataIndex: 'slg', sorter: (a, b) => (a.slg || 0) - (b.slg || 0), width: 68, render: rateCell, ...right },
+  { title: metricTitle('wOBA'), dataIndex: 'woba', sorter: (a, b) => (a.woba || 0) - (b.woba || 0), width: 76, render: rateCell, ...right },
+  { title: metricTitle('Whiff%'), dataIndex: 'whiffPct', sorter: (a, b) => a.whiffPct - b.whiffPct, width: 82, render: pctCell, ...right },
+  { title: metricTitle('PutAway%'), dataIndex: 'putAwayPct', sorter: (a, b) => (a.putAwayPct || 0) - (b.putAwayPct || 0), width: 96, render: pctCell, ...right },
 ]
 
 export default function PitchTypeTable({ data }) {
